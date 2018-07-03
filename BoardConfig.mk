fedb86e4fd5bc94d6e92f032a73e6e3b4aab2022
marc0601 <marckhalil06@gmail.com>
2018-07-03 03:02:23 +0200

    initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/samsung/j1pop3gjv
+
+TARGET_BOARD_PLATFORM := SC7727S
+TARGET_BOOTLOADER_BOARD_NAME := j1pop3gjv
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 9218048
+BOARD_FLASH_BLOCK_SIZE := 0
+BOARD_HAS_NO_REAL_SDCARD := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_RECOVERY_SWIPE := true
+BOARD_USES_MMCUTILS := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+TW_EXCLUDE_SUPERSU := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_HAS_DOWNLOAD_MODE := true
+TW_NO_REBOOT_BOOTLOADER := true
+include $(LOCAL_PATH)/kernel.mk
+include device/generic/twrpbuilder/BoardConfig32.mk
+
